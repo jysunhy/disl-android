5e3215cfd99336afa1fc93a8826e914d34f2ecb3
hy <sunhaiyangsjtu@gmail.com>
2013-06-01 14:27:43 +0200

    modify

--- setenv.sh
+++ setenv.sh
@@ -8,9 +8,9 @@ then
 	echo "PATH already set"
 else
 	echo "setting the PATH"
-	echo 'export ANDROID_PATH="'` pwd `'"' >> $Profile
-	echo 'export PATH="$PATH:$ANDROID_PATH/tools"' >> $Profile
-	echo 'export PATH="$PATH:$ANDROID_PATH/platform-tools"' >> $Profile
-	echo 'export PATH="$PATH:$ANDROID_PATH/build-tools/android-4.2.2"' >> $Profile
+	sudo echo 'export ANDROID_PATH="'` pwd `'"' >> $Profile
+	sudo echo 'export PATH="$PATH:$ANDROID_PATH/tools"' >> $Profile
+	sudo echo 'export PATH="$PATH:$ANDROID_PATH/platform-tools"' >> $Profile
+	sudo echo 'export PATH="$PATH:$ANDROID_PATH/build-tools/android-4.2.2"' >> $Profile
 	source $Profile
 fi
