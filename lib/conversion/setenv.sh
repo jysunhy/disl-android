9065885f590a1407a38bd9330e94bc95b4032edd
sunh <sunhaiyangsjtu@gmail.com>
2013-09-26 19:27:50 +0200

    readme

--- lib/conversion/setenv.sh
+++ lib/conversion/setenv.sh
@@ -1,3 +1,3 @@
 
-#export LIB_HOME=/home/sunh/disl-android/lib
-#export DX=/home/sunh/tdroid/out/host/linux-x86/bin/dx
+export LIB_HOME=/home/sunh/disl-android/lib
+export DX=/home/sunh/tdroid/out/host/linux-x86/bin/dx
