a28eeeeff0c35debbf95c8523773abb5268e6d71
Your Name <you@example.com>
2013-11-30 13:13:29 +0100

    remote-agent example

---
+++ prj/src/packages/apps/LongTest/jni/Map.h
@@ -0,0 +1,103 @@
+#ifndef _RE_MAP_H_
+#define _RE_MAP_H_
+
+#include <pthread.h>
+#include "Common.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+#define DEFAULT_MAP_SIZE 5
+
+template<class K, class V>
+class Map{
+	public:
+		Map(int capacity=DEFAULT_MAP_SIZE):b_capacity(capacity),b_occupied(0){
+			b_keys = new K[capacity];
+			b_values = new V[capacity];
+			pthread_mutex_init(&gl_mtx,NULL);
+		}
+		~Map(){
+			delete []b_keys;
+			delete []b_values;
+			pthread_mutex_destroy(&gl_mtx);
+		}
+		bool Exist(const K &key){
+	//		ALOG(LOG_DEBUG,"HAIYANG","in %s",__FUNCTION__);
+			ScopedMutex mtx(&gl_mtx);
+			return _Exist(key);
+		}
+		V& Get(const K &key){
+	//		ALOG(LOG_DEBUG,"HAIYANG","in %s",__FUNCTION__);
+			ScopedMutex mtx(&gl_mtx);
+			return _Get(key);
+		}
+		void Set(const K &key, const V &value) {
+	//		ALOG(LOG_DEBUG,"HAIYANG","in %s",__FUNCTION__);
+			ScopedMutex mtx(&gl_mtx);
+			_Set(key, value);
+		}
+		V& operator[](const K& key){
+	//		ALOG(LOG_DEBUG,"HAIYANG","in %s",__FUNCTION__);
+			ScopedMutex mtx(&gl_mtx);
+			return _Get(key);
+		}
+		void Print(){
+			DEBUG("PRINTING MAP: %s",__FUNCTION__);
+			for(int i = 0; i < b_occupied; i++){
+				DEBUG("\t %d:%d",(int)b_keys[i],(int)b_values[i]);
+			}
+		}
+	private:
+		bool _Exist(const K& key){
+			bool res = false;
+			for(int i = 0; i < b_occupied; i++){
+				if(b_keys[i] == key) {
+					res = true;
+					break;
+				}
+			}
+	//		ALOG(LOG_DEBUG,"HAIYANG","in %s %s",__FUNCTION__, res?"exists":"noexists");
+			return res;
+		}
+		V& _Get(const K &key){
+			for(int i = 0; i < b_occupied; i++){
+				if(b_keys[i] == key)
+					return b_values[i];
+			}
+			ERROR("should check exist before  use Get ");
+			return b_values[0];
+			//return NULL;
+		}
+		void _Set(const K &key, const V &value) {
+			if(_Exist(key)){
+				V &res = _Get(key);
+				res = value;
+				return;
+			}
+			if(b_occupied == b_capacity){
+				_DoubleCapacity();
+			}
+			b_keys[b_occupied] = key;
+			b_values[b_occupied] = value;
+			b_occupied++;
+			return;
+		}
+		void _DoubleCapacity(){
+			K* new_keys = new K[b_capacity*2];
+			V* new_values = new V[b_capacity*2];
+			memcpy(new_keys, b_keys, b_occupied * sizeof(K));
+			memcpy(new_values, b_values, b_occupied * sizeof(V));
+			delete []b_keys;
+			delete []b_values;
+			b_keys = new_keys;
+			b_values = new_values;
+			b_capacity *= 2;
+		}
+		int b_capacity;
+		int b_occupied;
+		K *b_keys;
+		V *b_values;
+		pthread_mutex_t gl_mtx;
+};
+
+#endif
---
+++ prj/src/packages/apps/LongTest/src/ch/usi/dag/dislre/REDispatch.java
@@ -0,0 +1,103 @@
+package ch.usi.dag.dislre;
+
+public class REDispatch {
+	public static void test(){
+		add(1,2);
+		registerMethod("ch.usi.dag.disl.test.dispatch.CodeExecuted.bytecodesExecuted");
+		short s = 0;
+		byte b = 127;
+		short cnt = 0;
+		String tmp = "def";
+		REDispatch tmp2 = new REDispatch();
+		
+		while(cnt < 2) {
+			try {
+			Thread.sleep(100);
+			}catch(Exception e){
+			}
+			cnt++;
+
+			analysisStart((short)29, b);
+			sendObject(new Thread());
+			sendObject(new REDispatch());
+			sendObject(tmp);
+			sendObject(tmp2);
+			sendObject(new String("adb"));
+			sendByte((byte)cnt);
+			analysisEnd();
+		}
+		//analysisStart(s,b);
+		sendBoolean(true);
+		sendByte(b);
+		sendChar('a');
+		sendShort(s);
+		sendInt(0);
+		sendLong(0);
+		sendObject(new Object());
+		sendObjectPlusData(new Object());
+	}
+	public static native int add(int a, int b);
+	/**
+	 * Register method and receive id for this transmission 
+	 * 
+	 * @param analysisMethodDesc
+	 * @return
+	 */
+	public static native short registerMethod(String analysisMethodDesc);
+	
+	/**
+	 * Announce start of an analysis transmission
+	 *
+	 * @param analysisMethodDesc remote analysis method id
+	 */
+	public static native void analysisStart(short analysisMethodId);
+	
+	/**
+	 * Announce start of an analysis transmission with total ordering (among
+	 * several threads) under the same orderingId
+	 *
+	 * @param analysisMethodId remote analysis method id
+	 * @param orderingId analyses with the same orderingId are guaranteed to
+	 *                   be ordered. Only non-negative values are valid.
+	 */
+	public static native void analysisStart(short analysisMethodId,
+			byte orderingId);
+
+	/**
+	 * Announce end of all analysis
+	 */
+	public static native void manuallyClose();
+	/**
+	 * Announce start of all analysis
+	 */
+	public static native void manuallyOpen();
+	/**
+	 * Announce end of an analysis transmission
+	 */
+	public static native void analysisEnd();
+
+	// allows transmitting types
+	public static native void sendBoolean(boolean booleanToSend);
+	public static native void sendByte(byte byteToSend);
+	public static native void sendChar(char charToSend);
+	public static native void sendShort(short shortToSend);
+	public static native void sendInt(int intToSend);
+	public static native void sendLong(long longToSend);
+	public static native void sendObject(Object objToSend);
+	public static native void sendObjectPlusData(Object objToSend);
+	
+	// Methods use similar logic as Float.floatToIntBits() and
+	// Double.doubleToLongBits() but implemented in the native code
+	// to avoid perturbation
+	public static native void sendFloat(float floatToSend);
+	public static native void sendDouble(double doubleToSend);
+	
+
+	static{
+		System.loadLibrary("shadowvm");
+	}
+	// TODO re - basic type array support
+	//  - send length + all values in for cycle - all in native code
+	//  PROBLEM: somebody can change the values from the outside
+	//   - for example different thread
+}
