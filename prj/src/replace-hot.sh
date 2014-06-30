d431427d252d074d79898944a49a4e72ebac9a17
haiyang <sunhaiyangsjtu@gmail.com>
2014-06-30 11:35:44 +0200

    a

--- prj/src/replace-hot.sh
+++ prj/src/replace-hot.sh
@@ -1,4 +1,5 @@
-cp frameworks/native/libs/binder/* ~/android-src/frameworks/native/libs/binder/
-cp frameworks/native/include/binder/* ~/android-src/frameworks/native/include/binder/
-cp frameworks/base/cmds/servicemanager/* ~/android-src/frameworks/base/cmds/servicemanager/
-cp bionic/libc/kernel/common/linux/binder.h ~/android-src/bionic/libc/kernel/common/linux/binder.h
+ANDROID_SRC_HOME=~/android-src
+cp frameworks/native/libs/binder/* $ANDROID_SRC_HOME/frameworks/native/libs/binder/
+cp frameworks/native/include/binder/* $ANDROID_SRC_HOME/frameworks/native/include/binder/
+cp frameworks/base/cmds/servicemanager/* $ANDROID_SRC_HOME/frameworks/base/cmds/servicemanager/
+cp bionic/libc/kernel/common/linux/binder.h $ANDROID_SRC_HOME/bionic/libc/kernel/common/linux/binder.h
