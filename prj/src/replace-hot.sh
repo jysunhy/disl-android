696997b841c4c4c10526e0bc9fa150f3831b70d6
haiyang <sunhaiyangsjtu@gmail.com>
2014-06-30 11:31:43 +0200

    script added

---
+++ prj/src/replace-hot.sh
@@ -0,0 +1,4 @@
+cp frameworks/native/libs/binder/* ~/android-src/frameworks/native/libs/binder/
+cp frameworks/native/include/binder/* ~/android-src/frameworks/native/include/binder/
+cp frameworks/base/cmds/servicemanager/* ~/android-src/frameworks/base/cmds/servicemanager/
+cp bionic/libc/kernel/common/linux/binder.h ~/android-src/bionic/libc/kernel/common/linux/binder.h
---
+++ prj/src/update-repo.sh
@@ -0,0 +1,4 @@
+cp ~/android-src/frameworks/native/libs/binder/*.cpp frameworks/native/libs/binder/
+cp ~/android-src/frameworks/native/include/binder/*.h frameworks/native/include/binder/
+cp ~/android-src/frameworks/base/cmds/servicemanager/binder* frameworks/base/cmds/servicemanager/
+cp ~/android-src/bionic/libc/kernel/common/linux/binder.h  bionic/libc/kernel/common/linux/binder.h
