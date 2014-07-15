e74285b916d68be13d5ac0a073bb3d8c40d2c4d1
sunh <sunhaiyangsjtu@gmail.com>
2014-07-15 20:26:38 +0200

    merge finished

--- prj/disldroid/src-disl-bypass/dynamic/ch/usi/dag/disl/dynamicbypass/DynamicBypass.java
+++ prj/disldroid/src-disl-bypass/dynamic/ch/usi/dag/disl/dynamicbypass/DynamicBypass.java
@@ -2,39 +2,43 @@ package ch.usi.dag.disl.dynamicbypass;
 
 public final class DynamicBypass {
 
-    private static final boolean debug = Boolean.getBoolean ("debug");
-
-
+    //private static final boolean debug = Boolean.getBoolean ("debug");
     //
 
     public static boolean isActive () {
-        return Thread.currentThread ().bypass;
+	    Thread currentThread = Thread.currentThread();
+	    if(currentThread == null)
+	        return true;
+        return currentThread.bypass;
     }
 
 
     public static void activate () {
-        if (debug) {
+        //if (debug) {
             // bypass should be disabled in this state
-            if (Thread.currentThread ().bypass) {
-                throw new RuntimeException (
-                    "fatal error: dynamic bypass activated twice");
-            }
-        }
-
-        Thread.currentThread ().bypass = true;
+        //    if (Thread.currentThread ().bypass) {
+        //        throw new RuntimeException (
+        //            "fatal error: dynamic bypass activated twice");
+        //    }
+        //}
+	    Thread currentThread = Thread.currentThread();
+	    if(currentThread!=null)
+	        currentThread.bypass = true;
     }
 
 
     public static void deactivate () {
-        if (debug) {
+        //if (debug) {
             // bypass should be enabled in this state
-            if (!Thread.currentThread ().bypass) {
-                throw new RuntimeException (
-                    "fatal error: dynamic bypass deactivated twice");
-            }
-        }
+        //    if (!Thread.currentThread ().bypass) {
+        //        throw new RuntimeException (
+        //            "fatal error: dynamic bypass deactivated twice");
+        //    }
+        //}
 
-        Thread.currentThread ().bypass = false;
+	    Thread currentThread = Thread.currentThread();
+        if(currentThread!=null)
+            currentThread.bypass = false;
     }
 
 }
