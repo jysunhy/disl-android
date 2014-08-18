05eeca64dea2d8ac22c3e69b127a2f97c7a44e04
shy <sunhaiyangsjtu@gmail.com>
2014-08-18 15:50:32 +0200

    detail the boundary type

--- prj/disldroid/src-analysis/ch/usi/dag/ipc/disl/MethodCallDiSLClass.java
+++ prj/disldroid/src-analysis/ch/usi/dag/ipc/disl/MethodCallDiSLClass.java
@@ -1,35 +1,13 @@
 package ch.usi.dag.ipc.disl;
 
-import ch.usi.dag.disl.annotation.After;
 import ch.usi.dag.disl.annotation.AfterReturning;
 import ch.usi.dag.disl.annotation.Before;
-import ch.usi.dag.disl.marker.BodyMarker;
 import ch.usi.dag.disl.marker.BytecodeMarker;
 import ch.usi.dag.icc.disl.CallContext;
 import ch.usi.dag.ipc.analysis.IPCAnalysisStub;
 
 /* for generating runtime stack */
-public class PermissionLocationDiSLClass {
-
-
-    /* every time entering a method */
-    @Before (
-        marker = BodyMarker.class,
-        scope = "*.*",
-        order = 1000)
-    public static void before_enter (final CallContext msc) {
-        IPCAnalysisStub.boundary_start (msc.thisMethodFullName ());
-    }
-
-    /* every time leaving a method */
-    @After (
-        marker = BodyMarker.class,
-        scope = "*.*",
-        order = 1000)
-    public static void after_enter (final CallContext msc) {
-        IPCAnalysisStub.boundary_end (msc.thisMethodFullName());
-    }
-
+public class MethodCallDiSLClass {
     /* every time before calling a method */
     @Before (
         marker = BytecodeMarker.class,
