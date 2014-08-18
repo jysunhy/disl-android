a426a73bd94823140f414e47b92b1e8b2e3e12e3
shy <sunhaiyangsjtu@gmail.com>
2014-08-18 15:48:06 +0200

    add boundary back

--- prj/disldroid/src-analysis/ch/usi/dag/ipc/disl/PermissionLocationDiSLClass.java
+++ prj/disldroid/src-analysis/ch/usi/dag/ipc/disl/PermissionLocationDiSLClass.java
@@ -1,8 +1,10 @@
 package ch.usi.dag.ipc.disl;
 
 import ch.usi.dag.disl.annotation.After;
+import ch.usi.dag.disl.annotation.AfterReturning;
 import ch.usi.dag.disl.annotation.Before;
 import ch.usi.dag.disl.marker.BodyMarker;
+import ch.usi.dag.disl.marker.BytecodeMarker;
 import ch.usi.dag.icc.disl.CallContext;
 import ch.usi.dag.ipc.analysis.IPCAnalysisStub;
 
@@ -27,22 +29,22 @@ public class PermissionLocationDiSLClass {
     public static void after_enter (final CallContext msc) {
         IPCAnalysisStub.boundary_end (msc.thisMethodFullName());
     }
-//
-//    /* every time before calling a method */
-//    @Before (
-//        marker = BytecodeMarker.class,
-//        args = "invokestatic, invokespecial, invokestatic, invokeinterface, invokevirtual")
-//    public static void beforeInvoke (final CallContext ac) {
-//        final String methodName = ac.getCallee ();
-//        IPCAnalysisStub.boundary_start (methodName);
-//    }
-//
-//    /* every time after calling a method */
-//    @AfterReturning (
-//        marker = BytecodeMarker.class,
-//        args = "invokestatic, invokespecial, invokestatic, invokeinterface, invokevirtual")
-//    public static void afterInvoke (final CallContext ac) {
-//        final String methodName = ac.getCallee ();
-//        IPCAnalysisStub.boundary_end (methodName);
-//    }
+
+    /* every time before calling a method */
+    @Before (
+        marker = BytecodeMarker.class,
+        args = "invokestatic, invokespecial, invokestatic, invokeinterface, invokevirtual")
+    public static void beforeInvoke (final CallContext ac) {
+        final String methodName = ac.getCallee ();
+        IPCAnalysisStub.boundary_start (methodName);
+    }
+
+    /* every time after calling a method */
+    @AfterReturning (
+        marker = BytecodeMarker.class,
+        args = "invokestatic, invokespecial, invokestatic, invokeinterface, invokevirtual")
+    public static void afterInvoke (final CallContext ac) {
+        final String methodName = ac.getCallee ();
+        IPCAnalysisStub.boundary_end (methodName);
+    }
 }
