27e710f4a3610ab9f8136efc9ab2d0e3b4cc294a
sunh <sunhaiyangsjtu@gmail.com>
2014-09-29 02:12:21 +0200

    empty

---
+++ prj/disldroid/src-analysis/ch/usi/dag/empty/LogOnlyDiSLClass.java
@@ -0,0 +1,17 @@
+package ch.usi.dag.empty.disl;
+
+import ch.usi.dag.disl.annotation.Before;
+import ch.usi.dag.disl.marker.BodyMarker;
+import ch.usi.dag.disl.staticcontext.MethodStaticContext;
+import ch.usi.dag.dislre.AREDispatch;
+
+
+public class LogOnlyDiSLClass {
+
+    @Before (
+        marker = BodyMarker.class,
+        scope = "java.lang.String.<clinit>")
+    public static void api_194 (final MethodStaticContext msc) {
+        AREDispatch.NativeLog ("test");
+    }
+}
