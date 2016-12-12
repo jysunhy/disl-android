614e0c193a1c90b2fe715857e7e0cee648a55dca
haiyang <sunhaiyangsjtu@gmail.com>
2016-12-12 14:43:07 +0100

    updated tag

--- prj/disldroid/src-analysis/ch/usi/dag/rv/logging/DefaultLog.java
+++ prj/disldroid/src-analysis/ch/usi/dag/rv/logging/DefaultLog.java
@@ -3,13 +3,14 @@ package ch.usi.dag.rv.logging;
 import android.util.Log;
 
 public class DefaultLog {
+    static String TAG = "RVTOOL-";
     public static void v(final String tag, final String args){
         final long tid = Thread.currentThread ().getId ();
-        Log.v(tag, "(Thread-"+tid+")" + args);
+        Log.v(TAG+tag, "(Thread-"+tid+")" + args);
     }
     public static void d(final String tag, final String args){
         final long tid = Thread.currentThread ().getId ();
-        Log.d(tag, "(Thread-"+tid+")" + args);
+        Log.d(TAG+tag, "(Thread-"+tid+")" + args);
     }
     public static long getTID(){
         return Thread.currentThread ().getId ();
