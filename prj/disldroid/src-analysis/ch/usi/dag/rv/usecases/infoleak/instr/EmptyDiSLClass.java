9263782ad009200a2b25852ade78bd8d7aa5d194
haiyang <sunhaiyangsjtu@gmail.com>
2016-12-31 14:12:30 +0100

    empty

---
+++ prj/disldroid/src-analysis/ch/usi/dag/rv/usecases/infoleak/instr/EmptyDiSLClass.java
@@ -0,0 +1,20 @@
+package ch.usi.dag.rv.usecases.infoleak.instr;
+
+import android.util.Log;
+import ch.usi.dag.disl.annotation.Before;
+import ch.usi.dag.disl.marker.BodyMarker;
+import ch.usi.dag.disl.staticcontext.MethodStaticContext;
+
+/*
+ * for generating runtime stack
+ * Take effects on app code
+ */
+public class EmptyDiSLClass {
+    @Before (
+        marker = BodyMarker.class,
+        order = 1000)
+    public static void before_enter (final MethodStaticContext msc) {
+        Log.d ("HAIYANG", msc.thisMethodFullName ());
+    }
+
+}
